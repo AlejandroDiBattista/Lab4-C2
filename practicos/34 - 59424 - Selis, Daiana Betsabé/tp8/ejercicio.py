e32fd4afc9e16a5708a73cf0008c6fe6d7c5fbce
daianaselis <100384776+daianaselis@users.noreply.github.com>
2024-11-26 18:27:33 -0300

    Update ejercicio.py

--- practicos/34 - 59424 - Selis, Daiana Betsabé/tp8/ejercicio.py
+++ practicos/34 - 59424 - Selis, Daiana Betsabé/tp8/ejercicio.py
@@ -14,9 +14,6 @@ archivo = st.sidebar.file_uploader("Selecciona un archivo CSV", type=["csv"])
 
 if archivo is not None:
     df = pd.read_csv(archivo)
-    
-    st.subheader(f"Vista previa de los datos cargados")
-    st.write(df.head())
 
     required_columns = ['Sucursal', 'Producto', 'Año','Unidades_vendidas','Ingreso_total','Costo_total']
     if not all (col in df.columns for col in required_columns):
@@ -28,13 +25,9 @@ if archivo is not None:
 
         productos = df['Producto'].unique()
 
-        df_grouped = df.groupby('Producto').agg(
-            Precio_promedio=('Ingreso_total', lambda x: x.sum() / df.loc[x.index, 'Unidades_vendidas'].sum()),
-            Margen_promedio=('Ingreso_total', lambda x: (x.sum() - df.loc[x.index, 'Costo_total'].sum()) / x.sum()),
-            Unidades_vendidas=('Unidades_vendidas', 'sum')
-        ).reset_index()
+        df['Precio_promedio'] = df['Ingreso_total'] / df['Unidades_vendidas']
 
-        last_month = df[['Año', 'Mes']].max()  # Find the latest year and month
+        last_month = df[['Año', 'Mes']].max() 
         last_year, last_month = last_month['Año'], last_month['Mes']
         if last_month == 1:
             previous_month = 12
@@ -42,26 +35,15 @@ if archivo is not None:
         else:
             previous_month = last_month - 1
             previous_year = last_year
-
-        df_filtered = df[(df['Año'] < last_year) | ((df['Año'] == last_year) & (df['Mes'] < last_month))]
-        # Aggregate the data until the last available month
-        df_grouped_filtered = df_filtered.groupby('Producto').agg(
-            Precio_promedio=('Ingreso_total', lambda x: x.sum() / df_filtered.loc[x.index, 'Unidades_vendidas'].sum()),
-            Margen_promedio=('Ingreso_total', lambda x: (x.sum() - df_filtered.loc[x.index, 'Costo_total'].sum()) / x.sum()),
-            Unidades_vendidas=('Unidades_vendidas', 'sum')
-        ).reset_index()
-    
-        st.subheader("Resumen por Producto")
-        st.write(df_grouped)
         
-        st.subheader("Evolución de las ventas por Mes")
+        st.subheader("Datos de " + (sucursal_opcion if sucursal_opcion != 'Todos' else 'Todas las Sucursales'))
         
         df_last_month = df[(df['Año'] == last_year) & (df['Mes'] == last_month)]
-        # Filter the data for the previous month
+      
         df_previous_month = df[(df['Año'] == previous_year) & (df['Mes'] == previous_month)]
-        # Get the sold units for each product in the last month
+    
         units_last_month = df_last_month[['Producto', 'Unidades_vendidas']]
-        # Get the sold units for each product in the previous month
+        
         units_previous_month = df_previous_month[['Producto', 'Unidades_vendidas']]
 
         for producto in productos:
@@ -72,37 +54,32 @@ if archivo is not None:
             with st.container(border=True):
                 col1, col2 = st.columns([1, 2])
                 with col1:
-                    producto_info = df_grouped[df_grouped['Producto'] == producto].iloc[0]
-                    producto_info_last = df_grouped_filtered[df_grouped_filtered['Producto'] == producto].iloc[0]
-                    
-                    ulm = units_last_month[units_last_month['Producto'] == producto].values[0][1]
-                    upm = units_previous_month[units_previous_month['Producto'] == producto].values[0][1]
-                    porcentajeCambioUnidades = (ulm - upm) / upm * 100 
-                    porcentajeCambioMargen = (producto_info['Margen_promedio'] - producto_info_last['Margen_promedio']) / producto_info_last['Margen_promedio'] * 100
-                    porcentajeCambioPrecio = (producto_info['Precio_promedio'] - producto_info_last['Precio_promedio']) / producto_info_last['Precio_promedio'] * 100
-
                     st.title(producto)
-                    st.write(f"**Unidades Vendidas**: {producto_info['Unidades_vendidas']:,d}")
-                    if(porcentajeCambioUnidades > 0):
-                        st.markdown(f'<span style="color:green">⬆{porcentajeCambioUnidades:.2f}%</span>', unsafe_allow_html=True)
-                    else:
-                        st.markdown(f'<span style="color:red">⬇{porcentajeCambioUnidades:.2f}%</span>', unsafe_allow_html=True)
+                    df_filtered = df[df["Producto"] == producto]
+                    precio_promedio = df_filtered['Precio_promedio'].mean()
 
-                    st.write(f"**Margen Promedio**: {producto_info['Margen_promedio']*100:.2f}%")
-                    if(porcentajeCambioMargen > 0):
-                        st.markdown(f'<span style="color:green">⬆{porcentajeCambioMargen:.2f}%</span>', unsafe_allow_html=True)
-                    else:
-                        st.markdown(f'<span style="color:red">⬇{porcentajeCambioMargen:.2f}%</span>', unsafe_allow_html=True)
+                    precio_promedio_anual = df_filtered.groupby('Año')['Precio_promedio'].mean()
+                    variacion_precio_promedio_anual = precio_promedio_anual.pct_change().mean() * 100
 
-                    st.write(f"**Precio Promedio**: ${producto_info['Precio_promedio']:,.2f}")
-                    if(porcentajeCambioPrecio > 0):
-                        st.markdown(f'<span style="color:green">⬆{porcentajeCambioPrecio:.2f}%</span>', unsafe_allow_html=True)
-                    else:
-                        st.markdown(f'<span style="color:red">⬇{porcentajeCambioPrecio:.2f}%</span>', unsafe_allow_html=True)
+                    df_filtered['Ganancia'] = df_filtered['Ingreso_total'] - df_filtered['Costo_total']
+                    df_filtered['Margen'] = (df_filtered['Ganancia'] / df_filtered['Ingreso_total']) * 100
+                    margen_promedio = df_filtered['Margen'].mean()
+
+                    margen_promedio_anual = df_filtered.groupby('Año')['Margen'].mean()
+                    variacion_margen_promedio_anual = margen_promedio_anual.pct_change().mean() * 100
+
+                    unidades_promedio = df_filtered['Unidades_vendidas'].mean()
+                    unidades_vendidas = df_filtered['Unidades_vendidas'].sum()
+
+                    unidades_por_año = df_filtered.groupby('Año')['Unidades_vendidas'].sum()
+                    variacion_anual_unidades = unidades_por_año.pct_change().mean() * 100
+
+                    st.metric("Precio Promedio", f"${precio_promedio:,.0f}".replace(",","."), f"{variacion_precio_promedio_anual:.2f}%")
+                    st.metric("Margen Promedio", f"{margen_promedio:,.0f}%".replace(",","."), f"{variacion_margen_promedio_anual:.2f}%")
+                    st.metric("Unidades Vendidas", f"{unidades_vendidas:,.0f}".replace(",","."), f"{variacion_anual_unidades:.2f}%")
                 with col2:           
-                    fig, (ax1) = plt.subplots(1, 1, figsize=(12, 15))
+                    fig, (ax1) = plt.subplots(1, 1, figsize=(10, 6))
 
-                    # 1. Ventas por producto a lo largo del tiempo
                     ventas_producto = df.groupby(['Año', 'Mes', 'Producto'])['Unidades_vendidas'].sum().reset_index()
                     data = ventas_producto[ventas_producto['Producto'] == producto]
                     data['Periodo'] = data['Año'].astype(str) + '-' + data['Mes'].astype(str)
@@ -115,18 +92,28 @@ if archivo is not None:
                     model.fit(X, y)
                     y_pred = model.predict(X)
 
-                    ax1.grid(True)
-
                     ax1.plot(data['Periodo'], y, label=producto)
-                    ax1.plot(X, y_pred, label='Tendencia (Regresión Lineal)', color='red', linestyle='--')
+                    ax1.plot(X, y_pred, label='Tendencia', color='red', linestyle='--')
 
-                    ax1.set_title('Ventas por Producto')
+                    ax1.set_title('Evolucion de ventas Mensual')
                     ax1.set_xlabel('Período (Año-Mes)')
                     ax1.set_ylabel('Unidades Vendidas')
 
-                    # ax1.set_ylim([0, 10000])
+                    
                     ax1.legend()
-                    plt.xticks(rotation=90)
+                    tick_positions = range(0, len(data), 12) 
+                    tick_labels = data["Año"].iloc[tick_positions]  
+                    plt.xticks(tick_positions, tick_labels)
+                    ax = plt.gca()  
+                    ax.set_xticks(range(len(X)))
+
+                    ytick_positions = range(4)
+                    ytick_labels = [(i + 1) * max(y) / 4 for i in ytick_positions]
+
+                    plt.ylim(0, None)
+
+                    plt.grid(which='both')
+
                     st.pyplot(plt)
             
     def mostrar_informacion_alumno():
