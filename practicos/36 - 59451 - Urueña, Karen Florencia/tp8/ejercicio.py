626f5fb9faec66259387d114e600a0a6a8561a1d
Karen <karenuruea2@gmail.com>
2024-11-26 14:44:43 -0300

    agrego url

--- practicos/36 - 59451 - Urueña, Karen Florencia/tp8/ejercicio.py
+++ practicos/36 - 59451 - Urueña, Karen Florencia/tp8/ejercicio.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+## ATENCION: Debe colocar la direccion en la que ha sido publicada la aplicacion en la siguiente linea\
+# url = 'https://modificacionparcial.streamlit.app/'
 # Configuración de la página
 st.set_page_config(page_title="Análisis de Ventas", layout="wide")
 
